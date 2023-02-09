0a29bf86d66b31257a65f5c4432dd6bd9c5bdda8
Nicat <59221034+Nicat-dcw@users.noreply.github.com>
2023-02-10 01:00:47 +0400

    Create Collection.js

---
+++ src/base/client/Collection.js
@@ -0,0 +1,9 @@
+import { BaseClient } from "./BaseClient.js";
+
+import { WebSocketManager } from "../websocket/WebSocketManager.js";
+
+import Storage from '@wumpjs/storage'
+export class Collection extends Storage {
+return Storage;
+}
+//cooming soon....
