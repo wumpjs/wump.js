d6dd83b41e09bf0879c38e6a71f0290368da12a8
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2023-02-17 18:35:15 +0300

    Create TextInputObject.js

---
+++ src/base/structures/builders/TextInput/TextInputObject.js
@@ -0,0 +1,20 @@
+
+
+
+export default {
+   custom_id: null,
+   required: null,
+   style: 1,
+   min_length: null,
+   max_length: null,
+   placeholder: null,
+   label: "",
+   value: null,
+   type: 4
+}
+
+
+export const TextInputStyle = {
+   Short: 1,
+   Paragraph: 2
+}
