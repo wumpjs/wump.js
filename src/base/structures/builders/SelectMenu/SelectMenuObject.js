37c8f7aed6ffc80c6de0fc00376e2b0d9cd241da
Nicat <59221034+Nicat-dcw@users.noreply.github.com>
2023-02-17 19:18:37 +0000

    SelectMenu Object Added

---
+++ src/base/structures/builders/SelectMenu/SelectMenuObject.js
@@ -0,0 +1,18 @@
+/**
+ * @readonly
+ */
+export default {
+    type: 3,
+    custom_id:null,
+    options:[
+        {
+        label:null,
+        value:null,
+        description:null,
+        emoji:{
+            name:null,
+            id:null
+        }
+        }
+    ]
+  };
