e4fe991177e3dabe2cdd9d138abfe81eed7262fb
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2023-04-23 11:51:33 +0300

    Create versionControl.js

---
+++ src/base/utils/versionControl.js
@@ -0,0 +1,17 @@
+import RexarTools from "rexar-tools";
+
+
+
+
+const versionControl = async(warnMessage) => {
+    const thisVersion = require("../../../package.json").version
+    const latestVersion = await RexarTools.npm("djs-extended-collectors").then((m) => m.version)
+    if(thisVersion !== latestVersion){
+        console.log(warnMessage)
+    } else {
+        return;
+    }
+}
+
+
+export default versionControl
