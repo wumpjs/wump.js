668276ff72090e3a19fc132f976a85621aeb5831
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2023-05-10 18:44:16 +0300

    Update versionControl.js

--- src/base/utils/versionControl.js
+++ src/base/utils/versionControl.js
@@ -1,11 +1,11 @@
-import RexarTools from "rexar-tools";
+import fetch from "node-fetch";
 
 
 
 
 const versionControl = async(warnMessage) => {
     const thisVersion = require("../../../package.json").version
-    const latestVersion = await RexarTools.npm("djs-extended-collectors").then((m) => m.version)
+    const latestVersion = await fetch("https://registry.npmjs.com/wump.js").then((data) => data["dist-tags"].latest)
     if(thisVersion !== latestVersion){
         console.log(warnMessage)
     } else {
