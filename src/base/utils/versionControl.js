d0f1dad379ca65b3ce829c23efd2341fb897baeb
Davut Özgür Süküti <80099012+davutozgursukuti4531@users.noreply.github.com>
2023-06-08 11:37:35 +0300

    Update versionControl.js

--- src/base/utils/versionControl.js
+++ src/base/utils/versionControl.js
@@ -1,11 +1,11 @@
-import fetch from "node-fetch";
+import { fetch, FetchResultTypes } from '@sapphire/fetch';
 
 
 
 
 const versionControl = async(warnMessage) => {
     const thisVersion = require("../../../package.json").version
-    const latestVersion = await fetch("https://registry.npmjs.com/wump.js").then((data) => data["dist-tags"].latest)
+    const latestVersion = await fetch("https://registry.npmjs.com/wump.js", FetchResultTypes.JSON).then((data) => data["dist-tags"].latest)
     if(thisVersion !== latestVersion){
         console.log(warnMessage)
     } else {
